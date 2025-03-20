42c1daf1ad92c33738eea89cbd75609cdfff2ffb
Jacques Duflos <31228096+JacquesDuflos@users.noreply.github.com>
2025-03-20 17:09:41 -0500

    probando el i2c
    l'adresse i2d a l'aire bone pourtant

--- Voltaje_Corriente/Voltaje_Corriente.ino
+++ Voltaje_Corriente/Voltaje_Corriente.ino
@@ -1,17 +1,24 @@
 #include <ArduinoJson.h>
-
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 float V1;
 float V2;
 float I1;
 float I2;
 
+LiquidCrystal_I2C lcd(0x27,  16, 2);
+
 void setup() {
-  // put your setup code here, to run once:
+  // Start serial comunication
   Serial.begin(9600);
+  // initialize lcd screen
+  lcd.init();
+  // turn on the backlight
+  lcd.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Getting the infos
   V1 = analogRead(A0);
   delay(5);
   V2 = analogRead(A1);
@@ -19,6 +26,13 @@ void loop() {
   I1 = analogRead(A2);
   delay(5);
   I2 = analogRead(A3);
+
+  // printing to LCD
+  lcd.setCursor(0,0);
+  lcd.print("V1, I1, P1");
+
+  lcd.setCursor(1,0);
+  lcd.print("V2, I2, P2");
 
   // Create the JSON document
   StaticJsonDocument<200> Json_enviar;
@@ -29,5 +43,5 @@ void loop() {
   serializeJson(Json_enviar, Serial);
   Serial.println();
 
-  delay(100);
+  delay(1000);
 }
