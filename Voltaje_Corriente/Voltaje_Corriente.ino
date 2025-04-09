2f5be271f5da0e83ec7e5476038a38da9023ee0c
Jacques Duflos <31228096+JacquesDuflos@users.noreply.github.com>
2025-04-09 08:55:06 -0500

    added buttons and energy calculation

--- Voltaje_Corriente/Voltaje_Corriente.ino
+++ Voltaje_Corriente/Voltaje_Corriente.ino
@@ -5,13 +5,44 @@ float V1;
 float V2;
 float I1;
 float I2;
+float P; // El poder, calculado a partir del v y i selecionado
+float E; // la energia, calculada integrando el poder
+unsigned long lastTime = 0; // to get the calculate the delta between 2 loops
+
+int SVState; // estado del boton de cambio de voltimetro
+int SIState; // estado del boton de cambio de amperimetro
+int RBState; // estado del boton de resetear
+int LastSVState = LOW; // estado presednete del boton de voltimetro
+int LastSIState = LOW; // estado presedente del boton de amperimetro
+int LastRBState = LOW; // estado presedente del boton de resetear
+String vDisplayed = "V1"; // la tension para mostrar en lcd
+String iDisplayed = "I1"; // la intensidad para mostrar en lcd
+String vCalc = ""; // la tension para calcular la energia
+String iCalc = ""; // la intensidad para calcular la energia
+
+const int Vmetro1 = A0;
+const int Vmetro2 = A1;
+const int Imetro1 = A2;
+const int Imetro2 = A3;
+const int SwitchV = 2;
+const int SwitchI = 3;
+const int ResetButton = 4;
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50; 
+
 String message;
 String vegal;
 String iegal;
+int sample = 5;
 
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 
 void setup() {
+  // Los pins de botones
+  pinMode(SwitchV, INPUT);
+  pinMode(SwitchI, INPUT);
+  pinMode(ResetButton, INPUT);
   // Start serial comunication
   Serial.begin(9600);
   // initialize lcd screen
@@ -37,15 +68,6 @@ void loop() {
   I2 = map (I2, 2500, 2685, 0, 1000);
   I2 = float(I2)/1000.0;
 
-  // printing to LCD
-  vegal = "V1=";
-  iegal = "I1=";
-  message = vegal + V1 + iegal + I1;
-  lcd.setCursor(0,0);
-  lcd.print(message);
-
-  lcd.setCursor(0,1);
-  lcd.print("V2, I2, P2");
 
   // Create the JSON document
   StaticJsonDocument<200> Json_enviar;
@@ -57,7 +79,99 @@ void loop() {
   serializeJson(Json_enviar, Serial);
   Serial.println();
 
-  delay(1000);
+  // Read the buttons
+  int reading = digitalRead(SwitchV);
+  if (reading != LastSVState){
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay){
+    if (reading != SVState){
+      SVState = reading;
+      if (SVState == HIGH){
+        if (vDisplayed == "V1"){
+          vDisplayed = "V2";
+        }
+        else{
+          vDisplayed = "V1";
+        }
+      }
+    }
+  }
+  reading = digitalRead(SwitchI);
+  if (reading != LastSIState){
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay){
+    if (reading != SIState){
+      SIState = reading;
+      if (SIState == HIGH){
+        if (iDisplayed == "I1"){
+          iDisplayed = "I2";
+        }
+        else{
+          iDisplayed = "I1";
+        }
+      }
+    }
+  }
+  reading = digitalRead(SwitchV);
+  if (reading != LastRBState){
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay){
+    if (reading != RBState){
+      RBState = reading;
+      if (RBState == HIGH){
+        vCalc = vDisplayed;
+        iCalc = iDisplayed;
+        E = 0;
+      }
+    }
+  }
+
+  // calculate P
+  if (vDisplayed == "V1"){
+    P = V1;
+  } else {
+    P = V2;
+  }
+  if (iDisplayed == "I1"){
+    P *= I1;
+  } else {
+    P*= I2;
+  }
+
+  // calculate E
+  if (iCalc != "" and vCalc != ""){
+    float dE;
+    if (iCalc == "I1"){
+      dE = I1;
+    } else {
+      dE = I2;
+    }
+    if (vCalc == "V1") {
+      dE *= V1;
+    } else {
+      dE *= V2;
+    }
+    dE *= millis() - lastTime;
+    E += dE;
+  }
+
+  // printing to LCD
+  char buffer[1];
+  buffer[0] = "1";
+  //sprintf(buffer, "%S", "a string");
+  //printf(buffer,"%s %f %s %f",
+  //    vDisplayed,Json_enviar[vDisplayed],iDisplayed,Json_enviar[iDisplayed]);
+  lcd.setCursor(0,0);
+  lcd.print(buffer);
+
+  lcd.setCursor(0,1);
+  lcd.print("V2, I2, P2");
+
+
+  delay(100);
 }
 
 float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
