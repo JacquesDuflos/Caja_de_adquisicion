85b15ba47a1c3e9fb91c3245952d0a9cdce55f70
Jacques Duflos <31228096+JacquesDuflos@users.noreply.github.com>
2025-04-16 12:12:51 -0500

    add the I calibration feature

--- Voltaje_Corriente/Voltaje_Corriente.ino
+++ Voltaje_Corriente/Voltaje_Corriente.ino
@@ -2,10 +2,99 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <stdio.h>
+
+template <int order> // order is 1 or 2
+class LowPass
+{
+  private:
+    float a[order];
+    float b[order+1];
+    float omega0;
+    float dt;
+    bool adapt;
+    float tn1 = 0;
+    float x[order+1]; // Raw values
+    float y[order+1]; // Filtered values
+
+  public:  
+    LowPass(float f0, float fs, bool adaptive){
+      // f0: cutoff frequency (Hz)
+      // fs: sample frequency (Hz)
+      // adaptive: boolean flag, if set to 1, the code will automatically set
+      // the sample frequency based on the time history.
+      
+      omega0 = 6.28318530718*f0;
+      dt = 1.0/fs;
+      adapt = adaptive;
+      tn1 = -dt;
+      for(int k = 0; k < order+1; k++){
+        x[k] = 0;
+        y[k] = 0;        
+      }
+      setCoef();
+    }
+
+    void setCoef(){
+      if(adapt){
+        float t = micros()/1.0e6;
+        dt = t - tn1;
+        tn1 = t;
+      }
+      
+      float alpha = omega0*dt;
+      if(order==1){
+        a[0] = -(alpha - 2.0)/(alpha+2.0);
+        b[0] = alpha/(alpha+2.0);
+        b[1] = alpha/(alpha+2.0);        
+      }
+      if(order==2){
+        float alphaSq = alpha*alpha;
+        float beta[] = {1, sqrt(2), 1};
+        float D = alphaSq*beta[0] + 2*alpha*beta[1] + 4*beta[2];
+        b[0] = alphaSq/D;
+        b[1] = 2*b[0];
+        b[2] = b[0];
+        a[0] = -(2*alphaSq*beta[0] - 8*beta[2])/D;
+        a[1] = -(beta[0]*alphaSq - 2*beta[1]*alpha + 4*beta[2])/D;      
+      }
+    }
+
+    float filt(float xn){
+      // Provide me with the current raw value: x
+      // I will give you the current filtered value: y
+      if(adapt){
+        setCoef(); // Update coefficients if necessary      
+      }
+      y[0] = 0;
+      x[0] = xn;
+      // Compute the filtered values
+      for(int k = 0; k < order; k++){
+        y[0] += a[k]*y[k+1] + b[k]*x[k];
+      }
+      y[0] += b[order]*x[order];
+
+      // Save the historical values
+      for(int k = order; k > 0; k--){
+        y[k] = y[k-1];
+        x[k] = x[k-1];
+      }
+  
+      // Return the filtered value    
+      return y[0];
+    }
+};
+
+// Filter instance
+LowPass<2> lp1(1,1e3,true);
+LowPass<2> lp2(1,1e3,true);
+
+
 float V1;
 float V2;
 float I1;
+float I1offset; // obtained by calibrating
 float I2;
+float I2offset; // obtained by calibrating
 float P; // El poder, calculado a partir del v y i selecionado
 float E; // la energia, calculada integrando el poder
 unsigned long lastTime = 0; // to get the calculate the delta between 2 loops
@@ -32,12 +121,17 @@ bool forceRefresh = false;
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50; 
+unsigned long iPushedTime = 0; // to detect if the i button was manained 
+bool isSampling = false;
+int nSample = 0; // the iteration of samle
+float sampleI1 = 0; // the sum of samples for I1
+float sampleI2 = 0; // the sum of samples for I2
+const int sampleSize = 50; // the total number of sample
 
 String line1;
 String line2;
 String vegal;
 String iegal;
-int sample = 5;
 
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 unsigned long lastRefresh = 0; // Last time the screen was updated
@@ -61,29 +155,53 @@ void loop() {
   // Getting the infos
   // the volts are sensed directly by analog input, so 0 to 1023 val are mapped to 0-5v
   V1 = mapfloat (analogRead(A0), 0, 1023, 0, 5);
-  delay(5);
+  //delay(5);
   V2 = mapfloat (analogRead(A1), 0, 1023, 0, 5);
-  delay(5);
+  //delay(5);
+
   // The intensity come from a ASC712 B05 sensor with a sensitivity of 185 mV / A
   // So I map from the 0-1023 to 0-5 then from 2.5 - 2.685 to 0-1A
-  I1 = map (analogRead(A2), 0, 1023, 0, 5000);
-  I1 = map (I1, 2500, 2685, 0, 1000);
-  I1 = float(I1)/1000.0;
-  delay(5);
-  I2 = map (analogRead(A3), 0, 1023, 0, 5000);
-  I2 = map (I2, 2500, 2685, 0, 1000);
-  I2 = float(I2)/1000.0;
-
+  int I1_analog = map (analogRead(A2), 0, 1023, 0, 5000);
+  I1_analog = map (I1_analog, 2500, 2685, 0, 1000);
+  float I1_unfilter = float(I1_analog)/1000.0;
+  int I2_analog =  map (analogRead(A3), 0, 1023, 0, 5000);
+  I2_analog = map (I2_analog, 2500, 2685, 0, 1000);
+  float I2_unfilter = float(I2_analog)/1000.0;
+  if (isSampling){
+    sampleI1 += I1_unfilter;
+    sampleI2 += I2_unfilter;
+    nSample ++;
+    if (nSample >= sampleSize){
+      I1offset = sampleI1 / sampleSize;
+      I2offset = sampleI2 / sampleSize;
+      isSampling = false;
+    }
+  }
+  else{
+    
+    I1_unfilter -= I1offset;
+    I1 = lp1.filt(I1_unfilter);
+    //delay(5);
+    
+    I2_unfilter -= I2offset;
+    I2 = lp2.filt(I2_unfilter);
+  }
 
   // Create the JSON document
   StaticJsonDocument<200> Json_enviar;
+ 
   Json_enviar["ProductName"] = "ModuloDidactico";
   Json_enviar["V1"] = V1;
   Json_enviar["V2"] = V2;
   Json_enviar["I1"] = I1;
   Json_enviar["I2"] = I2;
-  //serializeJson(Json_enviar, Serial);
-  //Serial.println();
+
+/*
+  Json_enviar["I_filtre"]= I1;
+  Json_enviar["I_non_filtre"] = I1_unfilter;
+*/
+  serializeJson(Json_enviar, Serial);
+  Serial.println();
 
   // Read the buttons
   forceRefresh = false;
@@ -117,14 +235,33 @@ void loop() {
   }
   if ((millis() - lastDebounceTime) > debounceDelay){
     if (reading != SIState){
+      // When a change of state is confirmed
       SIState = reading;
       if (SIState == HIGH){
-        forceRefresh = true;
-        if (iDisplayed == "I1"){
-          iDisplayed = "I2";
+        iPushedTime = millis();
+      }
+      else{
+        if (not isSampling){
+          forceRefresh = true;
+          if (iDisplayed == "I1"){
+            iDisplayed = "I2";
+          }
+          else{
+            iDisplayed = "I1";
+          }
         }
-        else{
-          iDisplayed = "I1";
+      }
+    }
+    else{
+      // when the state has not changed
+      if ((SIState == HIGH) and ((millis() - iPushedTime) > 1500)){
+        Serial.println("appuy prologe detecte");
+        if (not isSampling){
+          Serial.println("demarrage sampling");
+          isSampling = true;
+          nSample = 0;
+          sampleI1 = 0;
+          sampleI2 = 0;
         }
       }
     }
@@ -159,15 +296,20 @@ void loop() {
   }
   char buf_I[10];
   char buf_P[10];
-  if (iDisplayed == "I1"){
-    P *= I1;
-    dtostrf(I1, 4, 1, buf_I);
-  } else {
-    P*= I2;
-    dtostrf(I2, 4, 1, buf_I);
+  if (isSampling){
+    strcpy(buf_I, "calib");
+    strcpy(buf_P, "calib");
   }
-  dtostrf(P, 4, 1, buf_P);
-
+  else{
+    if (iDisplayed == "I1"){
+      P *= I1;
+      dtostrf(I1, 4, 1, buf_I);
+    } else {
+      P*= I2;
+      dtostrf(I2, 4, 1, buf_I);
+    }
+    dtostrf(P, 4, 1, buf_P);
+  }
   // calculate E
   if (iCalc != "" and vCalc != ""){
     float dE;
@@ -229,9 +371,7 @@ void loop() {
     lcd.print(ligne);
   }
 
-
-
-  delay(500);
+  delay(10);
 }
 
 float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
